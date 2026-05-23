541bd5934babfbcbfe68ec66acc01e24cec42481
BIG-ERG-A <alec.lavigne.lux@gmail.com>
2026-05-23 14:56:05 +0200

    cleaned up code for real this time

--- mouseInterpolaterLinux2.py
+++ mouseInterpolaterLinux2.py
@@ -7,9 +7,9 @@ from evdev import ecodes
 import serial
 import time
 
-ser = serial.Serial('/dev/ttyUSB0', 115200, timeout=1)
+#ser = serial.Serial('/dev/ttyUSB0', 115200, timeout=1)
 
-time.sleep(2)   # allow Arduino reset
+#time.sleep(2)   # allow Arduino reset
 
 counter = 0
 
@@ -29,14 +29,7 @@ yRel = 0
 xAbs = 0
 yAbs = 0
 
-xVel = 0
-yVel = 0
-
-xAcc = 0
-yAcc = 0
-
-
-device = evdev.InputDevice('/dev/input/event14') #change eventn to correct peripheral
+device = evdev.InputDevice('/dev/input/event19') #change eventn to correct peripheral
 
 def integrator(distance, time):       #returns velocity in mm/s
     return distance / time
@@ -45,7 +38,7 @@ def cpiToMM(dots):
     CPI = 1000
     return dots*25.4 / CPI
 
-print("xAbs:   |yAbs:    |xVel:    |yVel:    |xAcc:    |yAcc:    ")
+print("xAbs:   |yAbs:    ")
 
 for event in device.read_loop():
 
@@ -60,33 +53,12 @@ for event in device.read_loop():
         if event.code == evdev.ecodes.REL_X:            #if movement x
             xRel = cpiToMM(event.value)
             xAbs += xRel
-            if dt > 0:
-                xVel = integrator(xRel,dt)
-                xAcc = integrator(xVel,dt)      #currently incorrect
         
         elif event.code == evdev.ecodes.REL_Y:          #if movement y
             yRel = cpiToMM(event.value)
             yAbs += yRel
-            if dt > 0:
-                yVel = integrator(yRel,dt)
-                yAcc = integrator(yVel,dt)      #currently incorrect
-
 
     if event.type == evdev.ecodes.SYN_REPORT:           #if sync event happens i.e. every time mouse updates
 
-        currentTime = event.timestamp()
-
-        if prvTime is None:
-            prvTime = currentTime
-            
-        dt = currentTime - prvTime
-        prvTime = currentTime
-        counter += 1
-    # if prvTime - now > 0.2:
-    #     xRel = 0
-    #     yRel = 0
-    #     prvTime = now
-        if counter > 0:
-            ser.write(f'x:{xAbs}<,y:{yAbs}\n'.encode())
-            counter = 0
-            print(f"{xAbs:8.2f}| {yAbs:8.2f}| {xVel:8.2f}| {yVel:8.2f}| {xAcc:8.2f}| {yAcc:8.2f}")
+        #ser.write(f'x:{xAbs}<,y:{yAbs}\n'.encode())
+        print(f"{xAbs:8.2f}| {yAbs:8.2f}")
